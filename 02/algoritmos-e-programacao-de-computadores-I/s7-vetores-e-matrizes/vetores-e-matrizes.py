beb6436f92e1775e297d4358d53ffd70fa7ba61c
cirobtsta <cirobtsta@gmail.com>
2026-03-26 16:06:49 -0300

    vetores

---
+++ 02/algoritmos-e-programacao-de-computadores-I/s7-vetores-e-matrizes/vetores-e-matrizes.py
@@ -0,0 +1,18 @@
+
+
+
+# Listas Multidimensionais
+
+i = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(i[0][0])  # 1
+
+print(i[1][2])  # 2
+
+
+
+# Matriz Transposta
+
+def simetrica(m):
+    nlinhas = len(m)
+    ncolunas = len(m[0])
